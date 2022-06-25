2ddb944c6520500aad832d484dbea814d58b5023
Mateusz Polok <mateusz24polok@gmail.com>
2022-06-25 17:14:02 +0200

    prepare welcome layout

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout name="welcome">
+    <h1
+      class="text-gray-700 text-base"
+      :style="{
+        margin: '0px',
+      }"
+    >
+      Welcome view
+    </h1>
+  </NuxtLayout>
 </template>
---
+++ frontend/layouts/welcome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="welcome-background">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+  .welcome-background {
+    min-height: 100vh;
+    background-image: url("~/assets/img/workout.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    filter: brightness(0.3);
+  }
+</style>
