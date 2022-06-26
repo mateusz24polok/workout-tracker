1ae745e67e47300bfb619363e4a2cb83440f618f
Mateusz Polok <mateusz24polok@gmail.com>
2022-06-26 23:31:35 +0200

    frontend/add registration page with registration form

---
+++ frontend/components/Registration/RegistrationForm.vue
@@ -0,0 +1,79 @@
+<template>
+  <FormKit
+    type="form"
+    ref="registrationForm"
+    v-model="formData"
+    form-class="px-4 py-2"
+    :actions="false"
+    @submit="submitHandler"
+  >
+    <h2 class="text-3xl mt-2 mb-4">Register User</h2>
+    <FormKit
+      type="text"
+      name="name"
+      label="Your name"
+      placeholder="Name"
+      validation="required"
+    />
+    <FormKit
+      type="text"
+      name="email"
+      label="Your email"
+      placeholder="Email"
+      validation="required|email"
+    />
+    <div class="double">
+      <FormKit
+        type="password"
+        name="password"
+        label="Password"
+        validation="required|length:6|matches:/[^a-zA-Z]/"
+        :validation-messages="{
+          matches: 'Please include at least one symbol',
+        }"
+        placeholder="Your password"
+      />
+      <FormKit
+        type="password"
+        name="password_confirm"
+        label="Confirm password"
+        placeholder="Confirm password"
+        validation="required|confirm"
+      />
+    </div>
+    <div class="button-group flex">
+      <FormKit
+        type="button"
+        label="Switch to login"
+        :style="{ 'background-color': 'red' }"
+        @click="switchToLogin"
+      />
+      <FormKit type="button" label="Register" @click="submitForm" />
+    </div>
+  </FormKit>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const router = useRouter();
+const registrationForm = ref(null);
+const submitted = ref(false);
+const formData = ref({});
+
+const switchToLogin = () => {
+  router.push("/login");
+};
+
+const submitForm = () => {
+  const node = registrationForm.value.node;
+  node.submit();
+};
+
+const submitHandler = async () => {
+  // Let's pretend this is an ajax request:
+  await new Promise((r) => setTimeout(r, 1000));
+  submitted.value = true;
+
+  console.log("Submitted data", formData.value);
+};
+</script>
---
+++ frontend/pages/registration/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Modal
+    :close-on-overlay="false"
+    :is-close-button="false"
+    :additional-modal-content-classes="['entry-page-modal-content']"
+    :isModalOpen="true"
+  >
+    <RegistrationForm />
+  </Modal>
+</template>
+
+<script lang="ts" setup>
+import Modal from "@/components/common/Modal/Modal.vue";
+import RegistrationForm from "@/components/Registration/RegistrationForm.vue";
+</script>
+
+<style lang="scss" src="./registration.scss" />
