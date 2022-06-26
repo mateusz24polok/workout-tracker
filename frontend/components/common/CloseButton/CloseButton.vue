776cf38ae4962b3d2bf1920f6fc3ca133a061c21
Mateusz Polok <mateusz24polok@gmail.com>
2022-06-26 16:49:13 +0200

    add CloseButton component

---
+++ frontend/components/common/CloseButton/CloseButton.vue
@@ -0,0 +1,16 @@
+<template>
+  <button
+    class="text-zinc-400 font-bold"
+    :style="{
+      'font-size': size + 'px',
+    }"
+  >
+    X
+  </button>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  size: { type: Number, default: 24 },
+});
+</script>
