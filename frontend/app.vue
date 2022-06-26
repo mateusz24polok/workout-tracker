5616f7114b27b56ad11f00f16b574628738a3245
Mateusz Polok <mateusz24polok@gmail.com>
2022-06-26 23:30:44 +0200

    frontend/add formik-themes

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,12 +1,9 @@
 <template>
   <NuxtLayout name="welcome">
-    <h1
-      class="text-gray-700 text-base"
-      :style="{
-        margin: '0px',
-      }"
-    >
-      Welcome view
-    </h1>
+    <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script lang="ts" setup>
+  import "@formkit/themes/genesis";
+</script>
