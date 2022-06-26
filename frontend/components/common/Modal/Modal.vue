4cd367f658ee55578a1c58aa6d1b40b63af59e1c
Mateusz Polok <mateusz24polok@gmail.com>
2022-06-26 16:50:03 +0200

    add Modal component

---
+++ frontend/components/common/Modal/Modal.vue
@@ -0,0 +1,48 @@
+<template>
+  <teleport v-if="isModalOpen" to="body">
+    <div
+      @click.self="onOverlayClick"
+      class="modal__overlay"
+      :class="additionalOverlayClasses"
+    >
+      <div
+        class="modal__content rounded-lg min-w-300 bg-sky-100"
+        :class="additionalModalContentClasses"
+      >
+        <CloseButton
+          class="modal__content-close-button cursor-pointer absolute right-3 top-3"
+          v-if="isCloseButton"
+        />
+        <div class="modal__content-container m-3">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts" setup>
+import CloseButton from "@/components/common/CloseButton/CloseButton.vue";
+
+const props = defineProps({
+  isModalOpen: { type: Boolean, default: false },
+  isCloseButton: { type: Boolean, default: true },
+  closeOnOverlay: { type: Boolean, default: true },
+  additionalOverlayClasses: { type: [Object, Array] },
+  additionalModalContentClasses: { type: [Object, Array] },
+});
+
+const emit = defineEmits(["onClose"]);
+
+const onModalClose = () => {
+  emit("onClose");
+};
+
+const onOverlayClick = () => {
+  if (props.closeOnOverlay) {
+    onModalClose();
+  }
+};
+</script>
+
+<style lang="scss" src="./Modal.scss" />
