4c9218569fa3a7472a6f786e48dbed9534783735
Mateusz Polok <mateusz24polok@gmail.com>
2022-07-09 22:35:00 +0200

    backend/add config

---
+++ backend/config/config.example.ts
@@ -0,0 +1,10 @@
+// export const config = {
+//   dbHost: "localhost",
+//   dbUser: "root",
+//   dbPassword: "password",
+//   dbDatabase: "database",
+//   corsOrigin: "http://localhost:3000",
+//   jwtSecret: "some-secret-jwt",
+//   jwtExpiresIn: "10d",
+//   jwtCookieExpiresIn: "90",
+// };
